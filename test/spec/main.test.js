33eb87cc8081c38b4b6a86e724998ebb931fedbf
Scott <scott@sscotth.io>
2015-02-05 16:55:12 -0600

    beforeEach to clear the table

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -28,26 +28,6 @@ describe('getStock', function () {
   });
 });
 
-describe('addStockToTable', function () {
-  it('should add a row table', function () {
-    var stock = { Name: 'SuperCorp', Symbol: 'SCRP', LastPrice: 12.34 };
-    $('tr').length.should.equal(0);
-    addStockToTable(stock);
-    $('tr').length.should.equal(1);
-  });
-  it('should use stock data in the appended row', function () {
-    var stock = { Name: 'SuperCorp', Symbol: 'SCRP', LastPrice: 12.34 },
-        $row  = addStockToTable(stock),
-        $tds  = $row.find('td');
-
-    $tds.length.should.equal(4);
-    $($tds[0]).text().should.equal('SuperCorp');
-    $($tds[1]).text().should.equal('SCRP');
-    $($tds[2]).text().should.equal('12.34');
-    $($tds[3]).text().should.equal('12.34');
-  });
-});
-
 describe('getMultipleStocks', function () {
   it('should return mulitple stock objects', function (done) {
     getMultipleStocks(['AAPL', 'MSFT'], function (stocks) {
@@ -59,21 +39,48 @@ describe('getMultipleStocks', function () {
   });
 });
 
-describe('refreshStockPrices', function () {
-  it('should edit each stock in the table with a new price', function () {
-    var stocks = [
-                   { Symbol: 'AAPL', LastPrice: 12.45 },
-                   { Symbol: 'MSFT', LastPrice: 23.56 }
-                 ],
-        $tbody = $('tbody'),
-        $trs   = $('tr');
+describe('table', function () {
+  beforeEach(function () {
+    $('tbody').empty();
+  });
 
-    $tbody.append('<tr><td>Apple Inc</td><td>AAPL</td><td>12.34</td><td>12.34</td></tr>');
-    $tbody.append('<tr><td>Microsoft Corp</td><td>MSFT</td><td>23.45</td><td>23.45</td></tr>');
+  describe('refreshStockPrices', function () {
+    it('should edit each stock in the table with a new price', function () {
+      var stocks = [
+                     { Symbol: 'AAPL', LastPrice: 12.45 },
+                     { Symbol: 'MSFT', LastPrice: 23.56 }
+                   ],
+          $tbody = $('tbody'),
+          $trs;
 
-    refreshStockPrices(stocks);
+      $tbody.append('<tr><td>Apple Inc</td><td>AAPL</td><td>12.34</td><td>12.34</td></tr>');
+      $tbody.append('<tr><td>Microsoft Corp</td><td>MSFT</td><td>23.45</td><td>23.45</td></tr>');
+      $trs = $('tr');
 
-    $($($trs[0]).find('td')[3]).text().should.equal('12.45');
-    $($($trs[1]).find('td')[3]).text().should.equal('23.56');
+      refreshStockPrices(stocks);
+
+      $($($trs[0]).find('td')[3]).text().should.equal('12.45');
+      $($($trs[1]).find('td')[3]).text().should.equal('23.56');
+    });
+  });
+
+  describe('addStockToTable', function () {
+    it('should add a row table', function () {
+      var stock = { Name: 'SuperCorp', Symbol: 'SCRP', LastPrice: 12.34 };
+      $('tr').length.should.equal(0);
+      addStockToTable(stock);
+      $('tr').length.should.equal(1);
+    });
+    it('should use stock data in the appended row', function () {
+      var stock = { Name: 'SuperCorp', Symbol: 'SCRP', LastPrice: 12.34 },
+          $row  = addStockToTable(stock),
+          $tds  = $row.find('td');
+
+      $tds.length.should.equal(4);
+      $($tds[0]).text().should.equal('SuperCorp');
+      $($tds[1]).text().should.equal('SCRP');
+      $($tds[2]).text().should.equal('12.34');
+      $($tds[3]).text().should.equal('12.34');
+    });
   });
 });

8e9df837e951528a289526e2f1ff7f90e9059127
Scott <scott@sscotth.io>
2015-02-05 10:23:37 -0600

    first passing test using source file

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -6,3 +6,11 @@ describe('test suite', function () {
     false.should.be.false;
   });
 });
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});
+
+

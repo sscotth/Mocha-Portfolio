8162addfdb3b34afe12a78993ac287739ce42c9b
Scott <scott@sscotth.io>
2015-02-06 15:09:10 -0600

    remove html2js

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.html': ['html2js']
     },
 
 

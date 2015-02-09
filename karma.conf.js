f5ccd44c7c93b30c465d2be29280ce10c148c398
Scott <scott@sscotth.io>
2015-02-09 10:08:17 -0600

    add coveralls support

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
     },
 
     coverageReporter: {
-      type : 'html',
+      type : 'lcov',
       dir : 'coverage/'
     },
 
@@ -43,7 +43,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage', 'coveralls'],
 
 
     // web server port

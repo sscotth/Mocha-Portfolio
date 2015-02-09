1809c9a3483f642b0ab0d17ca2fa098db8ad1efd
Scott <scott@sscotth.io>
2015-02-09 12:27:09 -0600

    add new karma ci config

---
+++ karma.conf-ci.js
@@ -0,0 +1,105 @@
+// Karma configuration
+// Generated on Fri Feb 06 2015 12:01:05 GMT-0600 (CST)
+
+module.exports = function(config) {
+  'use strict';
+
+  var sauceLaunchers = {
+    SL_IE7: {
+      base: 'SauceLabs',
+      platform: 'Windows XP',
+      browserName: 'Internet Explorer',
+      version: '7'
+    },
+    SL_IE9: {
+      base: 'SauceLabs',
+      platform: 'Windows XP',
+      browserName: 'Internet Explorer',
+      version: '9'
+    },
+    SL_Opera: {
+      base: 'SauceLabs',
+      platform: 'Windows XP',
+      browserName: 'Opera',
+      version: '12'
+    },
+    SL_Safari: {
+      base: 'SauceLabs',
+      platform: 'Yosemite',
+      browserName: 'Safari',
+      version: '8'
+    }
+  };
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'public/js/**/*.js',
+      'test/spec/**/*.test.js',
+      'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/async/lib/async.js',
+      'bower_components/lodash/lodash.min.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'public/js/**/*.js': ['coverage']
+    },
+
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage', 'coveralls', 'saucelabs'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    customLaunchers: sauceLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS', 'Firefox', 'SL_IE7', 'SL_IE9', 'SL_Opera', 'SL_Safari'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
